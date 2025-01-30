9053c62ccdc6fb110929c27329e40cbd4a89f831
Pavel Sinitsin <spavelit@list.ru>
2025-01-30 13:07:14 +0300

    refactor: simplify HTTP client and remove unused logging functionality

--- v2/utils.go
+++ v2/utils.go
@@ -1,17 +1,13 @@
 package v2
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/ernesto-jimenez/httplogger"
+	"io"
+	"net/http"
+
 	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"time"
 )
 
 func validateResponse(requests []ResponseRequests) error {
@@ -25,9 +21,7 @@ func validateResponse(requests []ResponseRequests) error {
 	return result
 }
 
-var client = http.Client{
-	Transport: httplogger.NewLoggedTransport(http.DefaultTransport, newLogger()),
-}
+var client = http.Client{}
 
 type RespErrors struct {
 	Errors []struct {
@@ -44,7 +38,7 @@ func jsonReq[T any](req *http.Request) (*T, error) {
 	defer response.Body.Close()
 
 	var s T
-	payload, err := ioutil.ReadAll(response.Body)
+	payload, err := io.ReadAll(response.Body)
 	if err != nil {
 		return nil, errors.Wrap(err, "ioutil.ReadAll")
 	}
@@ -59,49 +53,4 @@ func jsonReq[T any](req *http.Request) (*T, error) {
 	}
 
 	return &s, nil
-}
-
-type httpLogger struct {
-	log *log.Logger
-}
-
-func newLogger() *httpLogger {
-	return &httpLogger{
-		log: log.New(os.Stderr, "log - ", log.LstdFlags),
-	}
-}
-
-func (l *httpLogger) LogRequest(req *http.Request) {
-	if req.Body != nil {
-		body, _ := ioutil.ReadAll(req.Body)
-		req.Body = ioutil.NopCloser(bytes.NewReader(body))
-
-		l.log.Printf(
-			"Request %s %s %s",
-			req.Method,
-			req.URL.String(),
-			body,
-		)
-	} else {
-		l.log.Printf(
-			"Request %s %s",
-			req.Method,
-			req.URL.String(),
-		)
-	}
-}
-
-func (l *httpLogger) LogResponse(req *http.Request, res *http.Response, err error, duration time.Duration) {
-	duration /= time.Millisecond
-	if err != nil {
-		l.log.Println(err)
-	} else {
-		l.log.Printf(
-			"Response method=%s status=%d durationMs=%d %s",
-			req.Method,
-			res.StatusCode,
-			duration,
-			req.URL.String(),
-		)
-	}
 }
